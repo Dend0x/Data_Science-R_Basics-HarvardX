c70ded18c9c2c19798ea1613a329a91d7b57d2cb
Dend0x <dannyryska05@gmail.com>
2025-05-31 16:01:01 +0200

    Section 1.4 Vectors

---
+++ 1.4_Vectors/Part_11_whole_numbers_and_numericals.R
@@ -0,0 +1,6 @@
+# Check the class of 1, assigned to the object a
+class(1)
+
+# Confirm that the class of 1L is an integer
+
+class(1L)
---
+++ 1.4_Vectors/Part_9_sequences_of_certain_lengths.R
@@ -0,0 +1,6 @@
+# Save the sequence in an object a
+a <- seq(1, 10, length.out=100)
+
+# Determine the class of a
+
+class(a)
